ac425abe1f33ae73a3234eb4521e8465f6a2e569
VYGAKOOL <vygakool@gmail.com>
2025-11-12 16:24:59 +0200

    fix4

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,10 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    expected = [5, 5, 5, 5, 6, 6]
-    result = split_integer(32, 6)
-    assert result == expected, (
-        f"Expected {expected}, but got {result}. "
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6], (
         "The list must be sorted, balanced, and sum to 32."
     )
 
