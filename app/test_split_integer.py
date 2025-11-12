9817be6b149156ebe28e027f9b6db332a04a2ce0
Nazar Iliasevych <101870603+VYGAKOOL@users.noreply.github.com>
2025-11-12 16:09:23 +0200

    Update test_split_integer.py
    fix

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(32, 6)) == 32, (
+    assert sum(split_integer(32, 6)) == [5, 5, 5, 5, 6, 6], (
         "the sum of the parts should be equal to 'value'"
     )
 
