00a3bfab268aaeeb5941e02b1da70336b4e2823b
VYGAKOOL <vygakool@gmail.com>
2025-11-12 16:21:00 +0200

    fix3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -9,6 +9,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
         "The list must be sorted, balanced, and sum to 32."
     )
 
+
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert split_integer(6, 2) == [3, 3], (
         "All parts should be equal each other "
